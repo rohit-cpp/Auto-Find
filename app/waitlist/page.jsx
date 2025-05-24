11819d8a808254f827f4945f13fffc17904611a9
Rohit <rrgawanderohit@gmail.com>
2025-05-25 02:55:32 +0530

    landing page done

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const WaitListPage = () => {
+  return (
+    <div className="mt-20 fixed w-full">
+      <div className="aspect-video w-full">
+        <iframe
+          style={{ width: "100%" }}
+          height="569"
+          src="https://www.create.xyz/app/9f171369-fa88-4045-8b62-2ddec2f9714e"
+          title="Waiting List Landing Page"
+          frameBorder="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default WaitListPage;
---
+++ components/home-search.jsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Camera } from "lucide-react";
+import { Button } from "./ui/button";
+
+const HomeSearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isImageSearchActive, setIsImageSearchActive] = useState(false);
+  const handleTextSubmit = () => {};
+  const handleImageSearch = () => {};
+  return (
+    <div>
+      <form onSubmit={handleTextSubmit}>
+        <div className="relative flex items-center">
+          <Input
+            type="text"
+            placeholder="Enter make, model, or use our AI Image Search.."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm"
+          />
+          <div className="absolute right-[100px]">
+            <Camera
+              size={35}
+              onClick={() => {
+                setIsImageSearchActive(!isImageSearchActive);
+              }}
+              className="cursor-pointer rounded-xl p-1.5"
+              style={{
+                background: isImageSearchActive ? "black" : "",
+                color: isImageSearchActive ? "white" : "",
+              }}
+            />
+          </div>
+          <Button type="submit" className="absolute right-2 rounded-full">
+            Search
+          </Button>
+        </div>
+      </form>
+      {isImageSearchActive && (
+        <div className="mt-4">
+          <form onSubmit={handleImageSearch}>
+            <div></div>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomeSearch;
